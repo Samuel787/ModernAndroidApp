f691e5261d987a0d91f974a2dc85a838b22608b3
Samuel787 <suther.david.samuel@gmail.com>
2022-05-26 15:12:16 +0800

    [MVVM Architecture] Implement the ViewModel

--- app/src/main/java/com/example/modernandroidapp/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/modernandroidapp/viewmodel/ListViewModel.kt
@@ -1,6 +1,28 @@
 package com.example.modernandroidapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.modernandroidapp.model.Country
 
 class ListViewModel: ViewModel() {
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf<Country>(
+            Country("CountryA"),
+            Country("CountryA")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
+
+
 }
