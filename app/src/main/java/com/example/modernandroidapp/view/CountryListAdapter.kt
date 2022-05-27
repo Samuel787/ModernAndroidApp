95cae9403070f074f88a28d691151654910235b8
Samuel787 <suther.david.samuel@gmail.com>
2022-05-27 20:39:30 +0800

    [Retrofit] Update the View

--- app/src/main/java/com/example/modernandroidapp/view/CountryListAdapter.kt
+++ app/src/main/java/com/example/modernandroidapp/view/CountryListAdapter.kt
@@ -2,17 +2,25 @@ package com.example.modernandroidapp.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.modernandroidapp.R
 import com.example.modernandroidapp.model.Country
+import com.example.modernandroidapp.util.getProgressDrawable
+import com.example.modernandroidapp.util.loadImage
 
 class CountryListAdapter(var countries: ArrayList<Country>):  RecyclerView.Adapter<CountryListAdapter.CountryViewHolder>() {
 
     class CountryViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val imageView = view.findViewById<ImageView>(R.id.imageView)
         private val countryName = view.findViewById<TextView>(R.id.name)
+        private val countryCapital = view.findViewById<TextView>(R.id.capital)
+        private val progressDrawable = getProgressDrawable(view.context)
         fun bind(country: Country) {
             countryName.text = country.countryName
+            countryCapital.text = country.capital
+            imageView.loadImage(country.flag, progressDrawable)
         }
     }
 
